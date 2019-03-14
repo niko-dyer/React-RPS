700aa4cb1c86cb90c250bbe8b4f2cc2fcf17d5c6
Niko Dyer <nikodyer44@gmail.com>
2019-03-14 15:38:07 -0600

    extra styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import RPS from './RPS'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Icon } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <Container className="App">
-        <Header as='h1'>Rock, Paper, Scissors</Header>
+        <Header as='h1' icon >
+          <Icon name='hand rock outline' circular />
+          <Header.Content>Rock, Paper, Scissors</Header.Content>
+        </Header>
+        <br />
         <span>Click a Hand to Make Your Choice</span>
         <hr />
         <RPS />
